a56b24b1392ac96a3cb7c40f71a6ac11dd19835b
eva.guegan <eva.guegan@hotmail.fr>
2021-02-01 13:50:18 +0100

    filtre recherche ok

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 var demo = new Vue({
     el: '#main',
     data: {
+        searchEvent: "",
         events: []
     },
     beforeCreate() {
         axios
-            .get('https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&q=&rows=10&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&facet=lieu_quartier')
+            .get('https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&q=&rows=10')
             .then(res => {
                 this.events = res.data.records
                 console.log(res.data.records)
             })
+    },
+    computed: {
+        eventSearch: function () {
+            var events_array = this.events,
+                searchEvent = this.searchEvent;
+
+            if(!searchEvent){
+                return events_array;
+            }
+
+            searchEvent = searchEvent.trim().toLowerCase();
+
+            events_array = events_array.filter(function(item){
+                if(item.fields.nom.toLowerCase().indexOf(searchEvent) !== -1){
+                    return item;
+                }
+            })
+
+            return events_array;
+        }
     }
 });
