5c43b6f86068057de3717aa2c4491256dffe3dda
eva.guegan <eva.guegan@hotmail.fr>
2021-02-01 11:12:42 +0100

    api ok

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ var demo = new Vue({
         axios
             .get('https://data.nantesmetropole.fr/api/records/1.0/search/?dataset=244400404_agenda-evenements-nantes-nantes-metropole&q=&rows=5&facet=emetteur&facet=rubrique&facet=lieu&facet=ville&facet=lieu_quartier')
             .then(res => {
-                this.events = res.data.results
-                console.log(res.data.results)
+                this.events = res.data.records
             })
     }
 });
